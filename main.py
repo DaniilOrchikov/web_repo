cd57f828279bcfde3f6496322e2a806b4fba3f5d
Student <danilaor2009@gmail.com>
2022-02-21 18:11:45 +0300

    Реклама с картинкой v2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
@@ -29,20 +29,7 @@ def image_mars():
 
 @app.route("/promotion_image")
 def promotion_image():
-    return '''<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">''' + "<h1>Жди нас, Марс!</h1>" + "</br>" + \
-           f'''<link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-           <img src="{url_for('static', filename='img/mars.jpg')}"
-           alt="здесь должна была быть картинка, но не нашлась">''' + '''<div class="alert alert-secondary" role="alert">
-  Человечество вырастает из детства
-</div>''' + '''<div class="alert alert-success" role="alert">
-  Человечеству мала одна планета
-</div>''' + '''<div class="alert alert-secondary" role="alert">
-  Мы сделаем обитаемыми безжизненные пока планеты
-</div>''' + '''<div class="alert alert-warning" role="alert">
-  И начнем с Марса!
-</div>''' + '''<div class="alert alert-danger" role="alert">
-  Присоединяйся!
-</div>'''
+    return render_template('index.html', title='Домашняя страница')
 
 
 if __name__ == "__main__":
