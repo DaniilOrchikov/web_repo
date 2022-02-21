ed21c0b400954221bd8eaa5b17e6a2c2a1f74ce3
Student <danilaor2009@gmail.com>
2022-02-21 20:06:54 +0300

    котья галерея

--- main.py
+++ main.py
@@ -1,40 +1,70 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return "Миссия Колонизация Марса"
-
-
 @app.route("/index")
-def countdown():
-    return "И на Марсе будут яблони цвести!"
+def index():
+    return render_template('index.html', name="Вася")
 
 
-@app.route("/promotion")
-def promotion():
-    return '</br>'.join([i for i in ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
-                                     'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!',
-                                     'Присоединяйся!']])
+@app.route("/profile")
+def profile():
+    return render_template('profile.html', name="Вася")
 
 
-@app.route("/image_mars")
-def image_mars():
-    return "<h1>Жди нас, Марс!</h1>" + "</br>" + \
-           f'''<img src="{url_for('static', filename='img/mars.jpg')}"
-           alt="здесь должна была быть картинка, но не нашлась">''' + '</br>Вот она какая, красная планета'
+@app.route("/login_form", methods=["GET", "POST"])
+def login_form():
+    if request.method == "POST":
+        return request.files["inputFile"].read()
+    elif request.method == "GET":
+        return render_template('login_form.html')
 
 
-@app.route("/promotion_image")
-def promotion_image():
-    return render_template('promotion_image.html')
-
-
-@app.route("/astronaut_selection")
-def astronaut_selection():
-    return render_template('astronaut_selection.html')
+@app.route("/no")
+def no():
+    return '''<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <h1>Все равно смотри на котов</h1><div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Zunge_raus.JPG/1200px-Zunge_raus.JPG" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="https://cdnn21.img.ria.ru/images/07e5/06/18/1738448523_0:54:864:540_1920x0_80_0_0_22bd72aa578b3fece6a89a620c95c4a1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://img.gazeta.ru/files3/749/14276749/2in1-2021new-_1_-k32opiya-pic_32ratio_900x600-900x600-72725.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>'''
 
 
 if __name__ == "__main__":
