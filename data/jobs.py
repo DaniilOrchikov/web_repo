59c612f75fa5c4fbd4b3362bf28aff09c430b7ac
danil <danilaor2009@gmail.com>
2022-03-25 18:03:17 +0300

    Первая работа

--- data/jobs.py
+++ data/jobs.py
@@ -4,11 +4,11 @@ import sqlalchemy
 from .db_session import SqlAlchemyBase
 
 
-class Jobs(SqlAlchemyBase):
+class Job(SqlAlchemyBase):
     __tablename__ = 'jobs'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    team_leader = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey("users.id"))
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     collaborators = sqlalchemy.Column(sqlalchemy.String)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import datetime
 from flask import Flask
 from data import db_session
+from data.jobs import Job
 from data.users import User
 
 app = Flask(__name__)
@@ -20,12 +22,25 @@ def add_user(surname, name, age, position, speciality, address, email):
     db_sess.commit()
 
 
+def add_job(team_leader, job, work_size, collaborators, is_finished):
+    new_job = Job()
+    new_job.team_leader = team_leader
+    new_job.job = job
+    new_job.work_size = work_size
+    new_job.collaborators = collaborators
+    new_job.is_finished = is_finished
+    db_sess = db_session.create_session()
+    db_sess.add(new_job)
+    db_sess.commit()
+
+
 def main():
     db_session.global_init("db/mars_explorer.db")
     add_user('Scott', 'Ridley', 21, 'captain', 'research engineer', 'module_1', 'scott_chief@mars.org')
     add_user('White', 'Max', 52, 'colonist', 'engineer', 'module_1', 'user_1@mars.org')
     add_user('Gray', 'Sasha', 19, 'colonist', 'engineer', 'module_1', 'user_2@mars.org')
     add_user('Green', 'Ben', 36, 'colonist', 'engineer', 'module_1', 'user_3@mars.org')
+    add_job(1, 'deployment of residential modules 1 and 2', 15, '2, 3', False)
     # app.run()
 
 
